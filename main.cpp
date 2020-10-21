0b3150b2249d2615084e9730a21acd8dc8f84b17
Jan Szataniak <jan.szataniak@gmail.com>
2020-10-21 12:05:23 +0200

    Rendering a square of static triangles

--- main.cpp
+++ main.cpp
@@ -2,40 +2,61 @@
 #include "GL/glu.h"
 #include "GL/glut.h"
 
-void MyDisplay(void) {
-    // The new scene
-    glLoadIdentity();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POLYGON);// OpenGLa state
-    glColor4f(1.0f, 0.0f, 0.0f, 1.0f);//Red;
-    glVertex2f(100.0f, 50.0f);
-    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);//Green
-    glVertex2f(450.0f, 400.0f);
-    glColor4f(0.0f, 0.0f, 1.0f, 1.0f);//Blue
-    glVertex2f(450.0f, 50.0f);
+void drawTriangle() {
+    glBegin(GL_POLYGON);
+    glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+    glVertex2f(0.0f, 0.0f);
+    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+    glVertex2f(100.0f, 0.0f);
+    glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+    glVertex2f(0.0f, 100.0f);
     glEnd();
-    // The end of scene
-    glFlush();//start processing buffered OpenGL routines
-}
-void MyInit(void) {
-    glClearColor(0.0, 0.0, 0.0, 0.0);//select clearing (background) color
-    /* initialize viewing values */
-    glViewport(0, 0, 300, 300);//window origin and size
-    glMatrixMode(GL_PROJECTION);//
-    glLoadIdentity();//=1
-    gluOrtho2D(0.0, 500.0*1.2, 0.0, 500.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();//=1
 }
 
-int main(int argc, char** argv) { //<- for normal API
+void drawQuarter() {
+    drawTriangle();
+    glTranslated(0,100, 0);
+    drawTriangle();
+    glTranslated(0, 100, 0);
+    drawTriangle();
+    glTranslated(100, -100, 0);
+    drawTriangle();
+    glTranslated(0, -100, 0);
+    drawTriangle();
+    glTranslated(100, 0, 0);
+    drawTriangle();
+}
+
+void MyDisplay() {
+    glLoadIdentity();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glShadeModel(GL_SMOOTH);
+    for (int i = 0; i < 4; ++i) {
+        glRotated(90 * i, 0, 0, 100);
+        drawQuarter();
+        glLoadIdentity();
+    }
+    glFlush();
+}
+
+void MyInit() {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glViewport(0, 0, 300, 300);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-500.0, 500.0, -500.0, 500.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer and RGBA
-    glutInitWindowSize(250, 250);//initial window size
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("My window");//create widnow, hello title bar
+    glutCreateWindow("My window");
     MyInit();
-    glutDisplayFunc(MyDisplay);//
-    glutMainLoop();//enter main loop and process events
+    glutDisplayFunc(MyDisplay);
+    glutMainLoop();
     return 0;
 }
