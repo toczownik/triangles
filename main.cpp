b3288bcc0aec36fb756f20b9bf6a8fb8021491ff
Jan Szataniak <jan.szataniak@gmail.com>
2020-10-31 16:30:02 +0100

    Final version

--- main.cpp
+++ main.cpp
@@ -1,30 +1,79 @@
 #include "GL/gl.h"
 #include "GL/glu.h"
 #include "GL/glut.h"
+#include <cmath>
+
+#define SIZE 100
+#define STRAIGHT_SPREAD (sqrt(2) * spread / 2)
+#define X_MIDDLE_SPREAD (spread * 5 / 4)
+#define Y_MIDDLE_SPREAD (spread / 4)
+#define X_OUTER_SPREAD (spread * 9 / 4)
+#define Y_OUTER_SPREAD Y_MIDDLE_SPREAD
+#define INITIAL_OFFSET 25
+
+int rotation = 0;
+int spread = 0;
+int spreadChange = 1;
 
 void drawTriangle() {
     glBegin(GL_POLYGON);
     glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-    glVertex2f(0.0f, 0.0f);
+    glVertex2f(-25.0f, -25.0f);
     glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-    glVertex2f(100.0f, 0.0f);
+    glVertex2f(75.0f, -25.0f);
     glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-    glVertex2f(0.0f, 100.0f);
+    glVertex2f(-25.0f, 75.0f);
     glEnd();
 }
 
-void drawQuarter() {
+void drawInner() {
+    glPushMatrix();
+    glTranslated(INITIAL_OFFSET + STRAIGHT_SPREAD, INITIAL_OFFSET + STRAIGHT_SPREAD, 0);
+    glRotated(rotation, 0, 0, 100);
     drawTriangle();
-    glTranslated(0,100, 0);
+    glPopMatrix();
+}
+
+void drawMiddle() {
+    glPushMatrix();
+    glTranslated(INITIAL_OFFSET + Y_MIDDLE_SPREAD, INITIAL_OFFSET + SIZE + X_MIDDLE_SPREAD, 0);
+    glRotated(rotation, 0, 0, 100);
     drawTriangle();
-    glTranslated(0, 100, 0);
+    glPopMatrix();
+    glPushMatrix();
+    glTranslated(INITIAL_OFFSET + SIZE + X_MIDDLE_SPREAD, INITIAL_OFFSET + Y_MIDDLE_SPREAD, 0);
+    glRotated(rotation, 0, 0, 100);
     drawTriangle();
-    glTranslated(100, -100, 0);
+    glPopMatrix();
+}
+
+void drawOuter() {
+    glPushMatrix();
+    glTranslated(INITIAL_OFFSET + Y_OUTER_SPREAD, INITIAL_OFFSET + 2 * SIZE + X_OUTER_SPREAD, 0);
+    glRotated(rotation, 0, 0, 100);
     drawTriangle();
-    glTranslated(0, -100, 0);
+    glPopMatrix();
+    glPushMatrix();
+    glTranslated(INITIAL_OFFSET + SIZE + 2 * STRAIGHT_SPREAD, INITIAL_OFFSET + SIZE + 2 * STRAIGHT_SPREAD, 0);
+    glRotated(rotation, 0, 0, 100);
     drawTriangle();
-    glTranslated(100, 0, 0);
+    glPopMatrix();
+    glTranslated(INITIAL_OFFSET + 2 * SIZE + X_OUTER_SPREAD, INITIAL_OFFSET + Y_OUTER_SPREAD, 0);
+    glRotated(rotation, 0, 0, 100);
     drawTriangle();
+    glPopMatrix();
+}
+
+void update(int) {
+    rotation += 1;
+    if (rotation >= 360) {
+        rotation = -360;
+    }
+    spread += spreadChange;
+    if (spread > 180 || spread < 0) {
+        spreadChange = -spreadChange;
+    }
+    glutPostRedisplay();
 }
 
 void MyDisplay() {
@@ -32,16 +81,23 @@ void MyDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
     glShadeModel(GL_SMOOTH);
     for (int i = 0; i < 4; ++i) {
-        glRotated(90 * i, 0, 0, 100);
-        drawQuarter();
-        glLoadIdentity();
+        glPushMatrix();
+        glRotated(90 * i + rotation, 0, 0, 100);
+        drawInner();
+        drawOuter();
+        glPopMatrix();
+        glPushMatrix();
+        glRotated(-90 * i - rotation, 0, 0, 100);
+        drawMiddle();
+        glPopMatrix();
     }
     glFlush();
+    glutTimerFunc(25, update, 0);
 }
 
 void MyInit() {
     glClearColor(0.0, 0.0, 0.0, 0.0);
-    glViewport(0, 0, 300, 300);
+    glViewport(0, 0, 500, 500);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-500.0, 500.0, -500.0, 500.0);
